167766cec8e27b6262bfcc0b3eb61ee7be04c3a6
simphiwek <konaolwethu@gmail.com>
2023-12-29 18:12:16 +0200

    generating pdf service

--- src/main/java/com/example/backend/controller/TemplateController.java
+++ src/main/java/com/example/backend/controller/TemplateController.java
@@ -3,21 +3,57 @@ package com.example.backend.controller;
 import com.example.backend.entity.Quotation;
 import com.example.backend.service.QuotationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.context.IContext;
+import org.xhtmlrenderer.pdf.ITextRenderer;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+
 @Controller
+@CrossOrigin("http://localhost:3000")
 public class TemplateController {
 
     @Autowired
     private QuotationService quotationService;
 
+    @Autowired
+    private TemplateEngine templateEngine;
+
     @GetMapping("/template/{quoteId}")
-    public String generateUserHtml(@PathVariable Long quoteId, Model model) {
+    public ResponseEntity<byte[]> generatePdf(@PathVariable Long quoteId, Model model) throws Exception {
         Quotation quote = quotationService.getQuoteById(quoteId);
-        model.addAttribute("quote", quote);
-        return "quote-template";
+
+        // Create Thymeleaf context and set model attributes
+        Context thymeleafContext = new Context();
+        thymeleafContext.setVariable("quote", quote);
+
+        // Render HTML template to a String
+        String htmlContent = templateEngine.process("quote-template", thymeleafContext);
+
+        // Generate PDF from HTML
+        ByteArrayOutputStream pdfOutputStream = new ByteArrayOutputStream();
+        ITextRenderer renderer = new ITextRenderer();
+        renderer.setDocumentFromString(htmlContent);
+        renderer.layout();
+        renderer.createPDF(pdfOutputStream);
+
+        // Set response headers
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PDF);
+        headers.setContentDispositionFormData("attachment", "quote.pdf");
+
+        return new ResponseEntity<>(pdfOutputStream.toByteArray(), headers, HttpStatus.OK);
     }
 }
